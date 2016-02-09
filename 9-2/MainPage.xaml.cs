9b472169fedb8b0699f37a9864317993bf031046
Juuso Minkkilä <K1756@student.jamk.fi>
2016-02-09 11:17:14 +0200

    Now with graphics!

---
+++ 9-2/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _9_2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void EuroButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ErrorTextBlock.Text = "";
+                double num = double.Parse(textBox.Text);
+                num = num / 5.94573;
+                textBox.Text = num.ToString("0.00") + " €";
+            }
+            catch(FormatException)
+            {
+                ErrorTextBlock.Text = "Invalid input";
+            }
+        }
+
+        private void MarkkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ErrorTextBlock.Text = "";
+                double num = double.Parse(textBox.Text);
+                num = num * 5.94573;
+                textBox.Text = num.ToString("0.00") + " mk";
+            }
+            catch (FormatException)
+            {
+                ErrorTextBlock.Text = "Invalid input";
+            }
+        }
+    }
+}
